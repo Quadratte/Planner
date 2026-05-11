d62f29cc96fe9496152a27c4db32603d25c3800e
Sergei Philippov <quadratte@gmail.com>
2026-05-11 12:48:11 +0300

    Initial Commit

---
+++ Planner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Planner
+//
+//  Created by Qd on 11.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
